ecd6e38b5ee0840b3f2aeae9e54ecc5bef05dfe0
Emanuele Torelli <mennetorelli@hotmail.it>
2020-02-07 17:19:10 +0100

    migliorato boundary manager

--- Assets/Scripts/ObjectManagers/BoundaryManager.cs
+++ Assets/Scripts/ObjectManagers/BoundaryManager.cs
@@ -7,19 +7,20 @@ public class BoundaryManager : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Vector3 originalScale = other.transform.localScale;
-        Vector3 initialPosition = other.GetComponent<GrabbableManager>().initialPosition;
-
         if (other.gameObject.tag == "Grabbable")
         {
+            Vector3 originalScale = other.transform.localScale;
+            Vector3 initialPosition = other.GetComponent<GrabbableManager>().initialPosition;
+            Quaternion initialRotation = other.GetComponent<GrabbableManager>().initialRotation;
+
             Sequence sequence = DOTween.Sequence();
             sequence.Append(other.transform.DOScale(new Vector3(0, 0, 0), 0.5f));
+            sequence.AppendCallback(() => other.gameObject.GetComponent<Rigidbody>().velocity = new Vector3());
             sequence.AppendCallback(() => other.transform.position = initialPosition);
+            sequence.AppendCallback(() => other.transform.rotation = initialRotation);
             sequence.Append(other.transform.DOScale(originalScale, 0.5f));
 
             DOTween.Play(sequence);
-
-            other.gameObject.GetComponent<Rigidbody>().velocity = new Vector3();
         }
     }
 }
--- Assets/Scripts/ObjectManagers/GrabbableManager.cs
+++ Assets/Scripts/ObjectManagers/GrabbableManager.cs
@@ -7,10 +7,13 @@ public class GrabbableManager : MonoBehaviour
 {
     [HideInInspector]
     public Vector3 initialPosition;
+    [HideInInspector]
+    public Quaternion initialRotation;
 
     private void Awake()
     {
         initialPosition = transform.position;
+        initialRotation = transform.rotation;
     }
 
 }
