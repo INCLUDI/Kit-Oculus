15a238cde159bd1f9621c630e4ae22aa27736750
Emanuele Torelli <mennetorelli@hotmail.it>
2020-02-07 16:58:53 +0100

    boundary manager

--- Assets/Scripts/ObjectManagers/BoundaryManager.cs
+++ Assets/Scripts/ObjectManagers/BoundaryManager.cs
@@ -8,15 +8,15 @@ public class BoundaryManager : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Vector3 originalScale = other.transform.localScale;
+        Vector3 initialPosition = other.GetComponent<GrabbableManager>().initialPosition;
 
         if (other.gameObject.tag == "Grabbable")
         {
             Sequence sequence = DOTween.Sequence();
-            sequence.Append(transform.DOScale(new Vector3(0, 0, 0), 0.5f));
-            //sequence.AppendCallback(() => other.GetComponent<GrabbableManager>().ResetPosition());
-            //sequence.Append(transform.DOScale(originalScale, 0.5f));
+            sequence.Append(other.transform.DOScale(new Vector3(0, 0, 0), 0.5f));
+            sequence.AppendCallback(() => other.transform.position = initialPosition);
+            sequence.Append(other.transform.DOScale(originalScale, 0.5f));
 
-            transform.DORestart();
             DOTween.Play(sequence);
 
             other.gameObject.GetComponent<Rigidbody>().velocity = new Vector3();
--- Assets/Scripts/ObjectManagers/GrabbableManager.cs
+++ Assets/Scripts/ObjectManagers/GrabbableManager.cs
@@ -5,55 +5,12 @@ using DG.Tweening;
 
 public class GrabbableManager : MonoBehaviour
 {
-    // private OVRGrabbable grabbable;
-
-    Vector3 initialPosition;
+    [HideInInspector]
+    public Vector3 initialPosition;
 
     private void Awake()
     {
         initialPosition = transform.position;
     }
 
-    /*void Start()
-    {
-        EventManager.StartListening("EnableInteraction", EnableGrabbable);
-        EventManager.StartListening("DisableInteraction", DisableGrabbable);
-        EventManager.StartListening("DisableObject", DisableObject);
-    }
-
-    private void EnableGrabbable()
-    {
-        grabbable.enabled = true;
-    }
-
-    private void DisableGrabbable()
-    {
-        EventManager.TriggerEvent("ReleaseObject");
-        grabbable.enabled = false;
-        // transform.DOMove(initialPosition, 1);
-    }
-
-    private void DisableObject()
-    {
-        GetComponent<Collider>().enabled = false;
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<OVRGrabbable>().enabled = false;
-        enabled = false;
-        DisableGrabbable();
-    }
-
-    private void OnDestroy()
-    {
-        EventManager.StopListening("EnableInteraction", EnableGrabbable);
-        EventManager.StopListening("DisableInteraction", DisableGrabbable);
-        EventManager.StopListening("DisableObject", DisableObject);
-    }*/
-
-
-    public void ResetPosition()
-    {
-        Debug.Log(transform.position);
-        transform.position = initialPosition;
-        Debug.Log(transform.position);
-    }
 }
