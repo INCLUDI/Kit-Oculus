81c0a4baae01cf6eb58a833e1b52c07ca33ab295
Emanuele Torelli <mennetorelli@hotmail.it>
2020-10-03 20:16:57 +0200

    firebase+google auth

--- Assets/Scripts/OculusManager.cs
+++ Assets/Scripts/OculusManager.cs
@@ -14,36 +14,7 @@ public class OculusManager : PlatformManager
 {
     protected override void SceneLoaded(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<SceneInstance> scenes)
     {
-        Vector3 playerPosition = new Vector3(
-            currentActivity.playerTransform.position.x, 
-            currentActivity.playerTransform.position.y, 
-            currentActivity.playerTransform.position.z);
-        Quaternion playerRotation = Quaternion.Euler(
-            currentActivity.playerTransform.rotation.x, 
-            currentActivity.playerTransform.rotation.y, 
-            currentActivity.playerTransform.rotation.z);
-        Player = Instantiate(Player, playerPosition, playerRotation);
-        
-        ActivityManager = Instantiate(ActivityManager);
-
-        Vector3 assistantPosition = new Vector3(
-            currentActivity.assistantTransform.position.x,
-            currentActivity.assistantTransform.position.y,
-            currentActivity.assistantTransform.position.z);
-        Quaternion assistantRotation = Quaternion.Euler(
-            currentActivity.assistantTransform.rotation.x,
-            currentActivity.assistantTransform.rotation.y,
-            currentActivity.assistantTransform.rotation.z);
-        Vector3 assistantScale = new Vector3(
-            currentActivity.assistantTransform.scale.x,
-            currentActivity.assistantTransform.scale.y,
-            currentActivity.assistantTransform.scale.z);
-        GameObject assistant = Instantiate(Resources.Load<GameObject>
-            ("VirtualAssistants/Prefabs/" + assistantsList[PlayerPrefs.GetInt("SelectedAssistantIndex")]),
-            assistantPosition, assistantRotation);
-        assistant.transform.localScale = assistantScale;
-
-        StatsManager = Instantiate(StatsManager);
+        base.SceneLoaded(scenes);
 
         GameObject[] teleportationAreas = GameObject.FindGameObjectsWithTag("Ground");
         foreach (GameObject teleportationArea in teleportationAreas)
@@ -60,7 +31,7 @@ public class OculusManager : PlatformManager
 
     public override void ActivityCompleted()
     {
-        SceneManager.LoadScene("MenuScene");
+        LoadMenuScene();
     }
 
     public override Type SelectableTriggerType()
